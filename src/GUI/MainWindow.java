f7f437971b86bd5446a380f3c2b3a63a186d0271
medkam <med.dekam@student.avans.nl>
2021-02-11 18:49:19 +0100

    Link between MainWindow and Data implemented
    The attributes of a Schedule are now shown, also now possible to click on a lesson in the window which prints it in the console

--- src/GUI/MainWindow.java
+++ src/GUI/MainWindow.java
@@ -1,5 +1,8 @@
 package GUI;
 
+import Data.Lesson;
+import Data.Schedule;
+import Data.Teacher;
 import javafx.animation.AnimationTimer;
 import javafx.scene.layout.Pane;
 import org.jfree.fx.FXGraphics2D;
@@ -10,15 +13,28 @@ import java.awt.*;
 import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 
 public class MainWindow extends ResizableCanvas
 {
-    private ArrayList<String> courses = new ArrayList<>();
-    private ArrayList<String> courseTeachers = new ArrayList<>();
-    private ArrayList<String> classStartTimes = new ArrayList<>();
-    private ArrayList<String> classRooms = new ArrayList<>();
+    private Schedule schedule;
+    private HashMap<Rectangle2D, Lesson> lessonShapes = new HashMap<>();
 
-    public MainWindow(Pane pane)
+    private String[] classrooms = new String[]{"Lokaal 1", "Lokaal 2", "Lokaal 3", "Lokaal 4", "Lokaal 5", "Lokaal 6", "Lokaal 7", "Lokaal 8"};
+    private ArrayList<String> allStartingTimes = new ArrayList<>();
+    private ArrayList<String> allEndingTimes = new ArrayList<>();
+
+    private int timeStampVerticalSpacing = 90;
+    private int timeStampHorizontalSpacing = 100;
+    private int amountOfTimeStamps = 9;
+    private int distanceOfTimeStampsFromTop = 100;
+    private int distanceOfTimeStampsFromLeft = 50;
+
+    private int distanceOfClassroomsFromLeft = distanceOfTimeStampsFromLeft + timeStampHorizontalSpacing;
+    private int distanceOfClassroomsFromTop = 30;
+    private int classroomTableWidth = 200;
+
+    public MainWindow(Pane pane, Schedule schedule)
     {
         super(new Resizable()
         {
@@ -28,36 +44,22 @@ public class MainWindow extends ResizableCanvas
                 // this needs to be here but isn't used
             }
         }, pane);
+        this.schedule = schedule;
         initialise();
         draw(new FXGraphics2D(getGraphicsContext2D()));
+
+        super.setOnMouseClicked(event ->
+        {
+            for (Rectangle2D rectangle2D : this.lessonShapes.keySet()) {
+                if (rectangle2D.contains(event.getX(), event.getY())) {
+                    System.out.println(this.lessonShapes.get(rectangle2D));
+                }
+            }
+        });
     }
 
     private void initialise()
     {
-        this.courses.add("2dGraphics");
-        this.courses.add("OOM");
-        this.courses.add("OGP");
-        this.courses.add("POC");
-        this.courses.add("GIT");
-
-        this.courseTeachers.add("Etiënne");
-        this.courseTeachers.add("Jessica");
-        this.courseTeachers.add("Edwin");
-        this.courseTeachers.add("Katja");
-        this.courseTeachers.add("Maurice");
-
-        this.classStartTimes.add("10:00 -");
-        this.classStartTimes.add("10:00 -");
-        this.classStartTimes.add("12:30 -");
-        this.classStartTimes.add("10:00 -");
-        this.classStartTimes.add("15:30 -");
-
-        this.classRooms.add("Lokaal 1");
-        this.classRooms.add("Lokaal 2");
-        this.classRooms.add("Lokaal 3");
-        this.classRooms.add("Lokaal 4");
-        this.classRooms.add("Lokaal 5");
-
         new AnimationTimer()
         {
             long last = -1;
@@ -77,68 +79,75 @@ public class MainWindow extends ResizableCanvas
                 }
             }
         }.start();
+
+        Collections.addAll(allStartingTimes, "09:00", "10:00", "11:00", "12:00", "12:30", "13:30", "14:30", "15:30", "16:30");
+        Collections.addAll(allEndingTimes, "10:00", "11:00", "12:00", "12:30", "13:30", "14:30", "15:30", "16:30", "17:30");
+
+        for (Lesson lesson : this.schedule.getLessons())
+        {
+            int classroomIndex = lesson.getClassroom().getClassroom();
+            int startingTimeIndex = this.allStartingTimes.indexOf(lesson.getFormatBeginTime());
+            int amountOfTimeBlocks = this.allEndingTimes.indexOf(lesson.getFormatEndTime()) - startingTimeIndex + 1;
+            if (classroomIndex != -1 && startingTimeIndex != -1)
+            {
+                int y = this.distanceOfTimeStampsFromTop + startingTimeIndex * this.timeStampVerticalSpacing;
+                int x = this.distanceOfClassroomsFromLeft + classroomIndex * this.classroomTableWidth;
+                this.lessonShapes.put(new Rectangle2D.Double(x, y, this.classroomTableWidth, this.timeStampVerticalSpacing * amountOfTimeBlocks), lesson);
+            }
+            else
+            {
+                System.out.println("classroomindex: " + classroomIndex + " startingTime index: " + lesson.getFormatBeginTime() + " " + startingTimeIndex);
+            }
+        }
     }
 
     public void draw(FXGraphics2D graphics2D)
     {
         graphics2D.setFont(new Font(Font.DIALOG, Font.PLAIN, 25));
 
-        ArrayList<String> allClassrooms = new ArrayList<>();
-        String[] classrooms = new String[] {"Lokaal 1", "Lokaal 2", "Lokaal 3", "Lokaal 4", "Lokaal 5", "Lokaal 6", "Lokaal 7", "Lokaal 8"};
-        Collections.addAll(allClassrooms, classrooms);
 
-        ArrayList<String> allStartingTimes = new ArrayList<>();
-        String[] startingTimes = new String[] {"9:00 -", "10:00 -", "11:00 -", "12:00 -", "12:30 -",
-                "13:30 -", "14:30 -", "15:30 -", "16:30 -"};
-        Collections.addAll(allStartingTimes, startingTimes);
-
-        String[] endingTimes = new String[] {"10:00", "11:00", "12:00", "12:30", "13:30", "14:30",
-                "15:30", "16:30", "17:30"};
-
-        int timeStampVerticalSpacing = 90;
-        int timeStampHorizontalSpacing = 100;
-        int amountOfTimeStamps = 9;
-        int distanceOfTimeStampsFromTop = 100;
-        int distanceOfTimeStampsFromLeft = 50;
-
-        int distanceOfClassroomsFromLeft = 200;
-        int distanceOfClassroomsFromTop = 30;
-        int classroomTableWidth = 200;
 
         // verticale lijnen van het tijdvak
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < 2; i++)
+        {
             int x = distanceOfTimeStampsFromLeft + i * timeStampHorizontalSpacing;
-            graphics2D.drawLine(x , distanceOfTimeStampsFromTop, x, distanceOfTimeStampsFromTop + timeStampVerticalSpacing * amountOfTimeStamps);
+            graphics2D.drawLine(x, distanceOfTimeStampsFromTop, x, distanceOfTimeStampsFromTop + timeStampVerticalSpacing * amountOfTimeStamps);
         }
         // horizontale lijnen van het tijdvlak
-        for (int i = 0; i < 10; i++) {
-            int y = (distanceOfTimeStampsFromTop +  + i * timeStampVerticalSpacing);
-            graphics2D.drawLine(distanceOfTimeStampsFromLeft , y, distanceOfTimeStampsFromLeft + 100, y);
+        for (int i = 0; i < 10; i++)
+        {
+            int y = (distanceOfTimeStampsFromTop + +i * timeStampVerticalSpacing);
+            graphics2D.drawLine(distanceOfTimeStampsFromLeft, y, distanceOfTimeStampsFromLeft + 100, y);
         }
         // begintijden tekenen
-        for (int i = 0; i < startingTimes.length; i++) {
-            graphics2D.drawString(startingTimes[i],distanceOfTimeStampsFromLeft + 10, distanceOfTimeStampsFromTop + 25 + i * timeStampVerticalSpacing);
+        for (int i = 0; i < allStartingTimes.size(); i++)
+        {
+            graphics2D.drawString(allStartingTimes.get(i) + " -", distanceOfTimeStampsFromLeft + 10, distanceOfTimeStampsFromTop + 25 + i * timeStampVerticalSpacing);
         }
         // eindtijden tekenen
-        for (int i = 0; i < startingTimes.length; i++) {
-            graphics2D.drawString(endingTimes[i],distanceOfTimeStampsFromLeft + 10, distanceOfTimeStampsFromTop + 50 + i * timeStampVerticalSpacing);
+        for (int i = 0; i < allStartingTimes.size(); i++)
+        {
+            graphics2D.drawString(allEndingTimes.get(i), distanceOfTimeStampsFromLeft + 10, distanceOfTimeStampsFromTop + 50 + i * timeStampVerticalSpacing);
+        }
+
+        // bovenste horizontale lijn van het rooster
+        graphics2D.drawLine(distanceOfClassroomsFromLeft, distanceOfClassroomsFromTop, distanceOfClassroomsFromLeft + classroomTableWidth * classrooms.length, distanceOfClassroomsFromTop);
+        // interne horizontale lijnen van het rooster
+        for (int i = 0; i < amountOfTimeStamps + 1; i++)
+        {
+            int y = distanceOfTimeStampsFromTop + i * timeStampVerticalSpacing;
+            graphics2D.drawLine(distanceOfClassroomsFromLeft, y, distanceOfClassroomsFromLeft + classroomTableWidth * classrooms.length, y);
         }
 
         // Tekenen van de lessen
-        for (int i = 0; i < this.courses.size(); i++) {
-            int classroomIndex = allClassrooms.indexOf(this.classRooms.get(i));
-            int startingTimeIndex = allStartingTimes.indexOf(this.classStartTimes.get(i));
-            if (classroomIndex != -1 && startingTimeIndex != -1) {
-                int y = distanceOfTimeStampsFromTop + startingTimeIndex * timeStampVerticalSpacing;
-                int x = distanceOfClassroomsFromLeft + classroomIndex * classroomTableWidth;
-                graphics2D.setColor(Color.GRAY);
-                graphics2D.fill(new Rectangle2D.Double(x, y, classroomTableWidth, timeStampVerticalSpacing));
-                graphics2D.setColor(Color.BLACK);
-                graphics2D.drawString(this.courses.get(i), x, y + 20);
-                graphics2D.drawString(this.courseTeachers.get(i), x, y + 50);
-            } else {
-                System.out.println("classroomindex: " + classroomIndex + " startingTime index: " + startingTimeIndex);
-            }
+        for (Rectangle2D rectangle2D : this.lessonShapes.keySet()) {
+            graphics2D.setColor(Color.GRAY);
+            graphics2D.fill(rectangle2D);
+            graphics2D.setColor(Color.BLACK);
+            Teacher teacher = this.lessonShapes.get(rectangle2D).getTeacher();
+            graphics2D.drawString(teacher.getSubject(), (int)rectangle2D.getX(), (int)(rectangle2D.getY() + 20));
+            graphics2D.drawString(teacher.getName(), (int)rectangle2D.getX(), (int)(rectangle2D.getY() + 50));
+            graphics2D.drawString(this.lessonShapes.get(rectangle2D).getGroupNames(this.lessonShapes.get(rectangle2D).getGroups()), (int)rectangle2D.getX(), (int)(rectangle2D.getY() + 80));
         }
 
         // pauze tekenen
@@ -146,25 +155,28 @@ public class MainWindow extends ResizableCanvas
         graphics2D.fill(new Rectangle2D.Double(distanceOfClassroomsFromLeft, distanceOfTimeStampsFromTop + 3 * timeStampVerticalSpacing, classrooms.length * classroomTableWidth, timeStampVerticalSpacing));
         graphics2D.setColor(Color.BLACK);
         // Bij elk lokaal pauze tekenen
-        for (int i = 0; i < classrooms.length; i++) {
+        for (int i = 0; i < classrooms.length; i++)
+        {
             graphics2D.drawString("Pauze", distanceOfClassroomsFromLeft + i * classroomTableWidth, distanceOfTimeStampsFromTop + 30 + 3 * timeStampVerticalSpacing);
         }
 
-        // verticale lijnen van het rooster
-        for (int i = 0; i < classrooms.length + 1; i++) {
-            int x = distanceOfClassroomsFromLeft + i * classroomTableWidth;
-            graphics2D.drawLine(x , distanceOfClassroomsFromTop, x, distanceOfTimeStampsFromTop + amountOfTimeStamps * timeStampVerticalSpacing);
+        // lijn boven de pauzes opnieuw tekenen om te scheiden van de lessen.
+        for (int i = 0; i < 2; i++)
+        {
+            int y = distanceOfTimeStampsFromTop + 3 * timeStampVerticalSpacing + i * timeStampVerticalSpacing;
+            graphics2D.drawLine(distanceOfClassroomsFromLeft, y, distanceOfClassroomsFromLeft + classroomTableWidth * classrooms.length, y);
         }
-        // bovenste horizontale lijn van het rooster
-        graphics2D.drawLine(distanceOfClassroomsFromLeft , distanceOfClassroomsFromTop, distanceOfClassroomsFromLeft + classroomTableWidth * classrooms.length, distanceOfClassroomsFromTop);
-        // interne horizontale lijnen van het rooster
-        for (int i = 0; i < amountOfTimeStamps + 1; i++) {
-            int y = distanceOfTimeStampsFromTop + i * timeStampVerticalSpacing;
-            graphics2D.drawLine(distanceOfClassroomsFromLeft , y, distanceOfClassroomsFromLeft + classroomTableWidth * classrooms.length, y);
+
+        // verticale lijnen van het rooster
+        for (int i = 0; i < classrooms.length + 1; i++)
+        {
+            int x = distanceOfClassroomsFromLeft + i * classroomTableWidth;
+            graphics2D.drawLine(x, distanceOfClassroomsFromTop, x, distanceOfTimeStampsFromTop + amountOfTimeStamps * timeStampVerticalSpacing);
         }
 
         // alle lokalen tekenen
-        for (int i = 0; i < classrooms.length; i++) {
+        for (int i = 0; i < classrooms.length; i++)
+        {
             graphics2D.drawString(classrooms[i], distanceOfClassroomsFromLeft + i * classroomTableWidth, distanceOfClassroomsFromTop + 20);
         }
     }
