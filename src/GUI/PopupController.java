a1b80bf4ed55ec6c6e443cdca211f626616c047a
Ewout <EwoutBaars@live.nl>
2021-02-15 10:58:50 +0100

    Docententablad toegevoegd, en een textEditor gemaakt voor labels

--- src/GUI/PopupController.java
+++ src/GUI/PopupController.java
@@ -1,6 +1,7 @@
 package GUI;
 
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.stage.Stage;
@@ -45,6 +46,44 @@ public class PopupController
         stage.setTitle("PopupController");
 
         return stage;
+    }
+
+
+    public static void textEditor(Label label, int line, String string)
+    {
+
+        if (line < 0)
+        {
+            throw new IllegalArgumentException("line cant be below zero");
+        }
+
+
+
+            String text = label.getText();
+            String[] lines = text.split("\n");
+
+            StringBuilder newText = new StringBuilder();
+
+
+            for (int j = 0; j < line + 1; j++)
+            {
+                if (j == line)
+                {
+                    newText.append(string).append("\n");
+                }
+                else if (j > lines.length - 1)
+                {
+                    newText.append("\n");
+                }
+                else
+                {
+                    newText.append(lines[j]).append("\n");
+                }
+
+            }
+
+            label.setText(String.valueOf(newText));
+
     }
 
 
